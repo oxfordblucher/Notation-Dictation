93ff2b2e1bc83cdbe4e054664915852bbe53803d
oxfordblucher <hiuchanhk@gmail.com>
2020-11-11 15:28:31 -0500

    working id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 const noteData = require("../db/db");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+String.prototype.decapitalize = function() {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+}
+
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        res.json(noteData);
+        readFileAsync(path.join(__dirname, "../db/db.json"), "utf8")
+            .then(function(data) {
+                return res.json(JSON.parse(data));
+            })
     });
-
-    app.get("/api/notes/:note", function(req, res) {
-        const chosen = req.params.note.capitalize();
-        console.log(chosen)
-
-        for (let i = 0; i < noteData.length; i++) {
-            if (chosen === noteData[i].title) {
-                res.json(noteData[i]);
-            }
-            
-        }
-    })
 
     app.post("/api/notes", function(req, res) {
-        const text = req.body.text;
-        const title = req.body.title.capitalize();
-        
-        noteData.push({title, text});
+        const newNote = req.body;
+        newNote.id = parseInt(noteData.length);
+        readFileAsync(path.join(__dirname, "../db/db.json"), "utf8")
+            .then(function(data) {
+                let allNotes = JSON.parse(data);
+                allNotes.push(newNote);
+                writeFileAsync(path.join(__dirname, "../db/db.json"), JSON.stringify(allNotes));
+                return res.json(noteData);
+            })
     });
 
-    
+    app.delete("/api/notes/:id", function(req, res) {
+        let noteID = req.params.id;
+        for (let i = 0; i < noteData.length; i++) {
+            const selNote = noteData[i];
+            if (selNote.id == noteID) {
+                noteData.splice(i, 1);
+                i--;
+            }
+        }  
+        writeFileAsync(path.join(__dirname, "../db/db.json"), JSON.stringify(noteData))
+        return res.json(noteData);
+    })
 }
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const db = require("./db/db");
 
 const app = express();
 const PORT = 3777;
