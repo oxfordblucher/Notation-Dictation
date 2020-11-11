f5684bd322bf1763366df2a0f1882846d06d3185
oxfordblucher <hiuchanhk@gmail.com>
2020-11-11 11:05:10 -0500

    working html routes

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 const express = require("express");
-const fs = require("fs");
-const http = require("http");
 
-const PORT = 3333;
-const server = http.createServer(handleRequest);
+const app = express();
+const PORT = 3777;
 
-function handleRequest(req, res) {
-    let path = req.url;
-    switch (path) {
-        case "/public/index":
-        case "/public/notes":
-            return renderHTML(path + ".html", res);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-        default:
-            return renderHTML("/public/index.html", res);
-    }
-}
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-function renderHTML(filePath, res) {
-    return fs.readFile(__dirname + filePath, function(err, data) {
-        if(err) throw err;
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(data);
-    })
-}
-
-server.listen(PORT, function() {
-    console.log(`Server is listening on PORT: ${PORT}`)
+app.listen(PORT, function() {
+    console.log(`App listening on http://localhost:${PORT}`);
 })
