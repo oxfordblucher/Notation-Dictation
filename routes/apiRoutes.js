12f778d2dc105adde506265a6aa7c6854d134fde
oxfordblucher <hiuchanhk@gmail.com>
2020-11-11 13:12:35 -0500

    working apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,31 @@
 const noteData = require("../db/db");
 
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(noteData);
     });
 
+    app.get("/api/notes/:note", function(req, res) {
+        const chosen = req.params.note.capitalize();
+        console.log(chosen)
+
+        for (let i = 0; i < noteData.length; i++) {
+            if (chosen === noteData[i].title) {
+                res.json(noteData[i]);
+            }
+            
+        }
+    })
+
     app.post("/api/notes", function(req, res) {
-        noteData.push(req.body);
+        const text = req.body.text;
+        const title = req.body.title.capitalize();
+        
+        noteData.push({title, text});
     });
 
     
